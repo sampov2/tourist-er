8c0ab97646286726e26a0b59c27e1665c2e9fee6
Sampo Savolainen <sampo.savolainen@gmail.com>
2023-11-03 17:49:32 +0200

    wip: use imposm3 to import data into postgis

--- containers/geoserver/setup-server.sh
+++ containers/geoserver/setup-server.sh
@@ -2,23 +2,23 @@
 
 set -e
 
-export AUTH="admin:bNJNeijxW8U1M0H"
-export API="http://localhost:32769/geoserver/rest"
+export AUTH="admin:$(cat /run/secrets/GEOSERVER_ADMIN_PASSWORD)"
+export API="http://localhost:8080/geoserver/rest"
 
-#curl -i -X POST $API/workspaces -u $AUTH -H "Content-Type: application/json" \
-#    -d '{"workspace":{"name":"hello","href":"http://www.osm.com"}}'
+curl -i -X POST $API/workspaces -u $AUTH -H "Content-Type: application/json" \
+    -d '{"workspace":{"name":"osm","href":"http://www.osm.com"}}'
 
-read -r -d '' VAR <<- EOM
-{
-    "dataStore": {
-        "name": "nyc",
-        "connectionParameters": {
-            "entry": [
-                {"@key":"database","$":"file:///path/to/nyc.gpkg"},
-                {"@key":"dbtype","$":"geopkg"}
-            ]
-        }
-    }
-}
-EOM
-echo $VAR
+#read -r -d '' VAR <<- EOM
+#{
+#    "dataStore": {
+#        "name": "nyc",
+#        "connectionParameters": {
+#            "entry": [
+#                {"@key":"database","$":"file:///path/to/nyc.gpkg"},
+#                {"@key":"dbtype","$":"geopkg"}
+#            ]
+#        }
+#    }
+#}
+#EOM
+#echo $VAR
--- containers/postgis/init.sh
+++ containers/postgis/init.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+IMPOSM_VERSION="0.11.1"
+
 set -e
 
-echo 'localhost:5432:gis:docker:'$(cat /tmp/PGPASSWORD.txt) > $HOME/.pgpass
+echo 'localhost:5432:gis:docker:'$(cat /run/secrets/POSTGRES_PASS) > $HOME/.pgpass
 chmod 0600 $HOME/.pgpass
 
-osm2pgsql --host=localhost --port=5432 --database=gis --user=docker /tmp/input.pbf
+wget -O /tmp/imposm.tar.gz https://github.com/omniscale/imposm3/releases/download/v$IMPOSM_VERSION/imposm-$IMPOSM_VERSION-linux-x86-64.tar.gz
+
+cd /tmp/
+tar xf imposm.tar.gz
+
+IMPOSM_DIR=/tmp/imposm-$IMPOSM_VERSION-linux-x86-64
+IMPOSM_MAPPING=$IMPOSM_DIR/mapping.json
+
+POSTGIS_CONNECTION=postgis://docker:$(cat /run/secrets/POSTGRES_PASS)@localhost:5432/gis
+
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -read /tmp/input.pbf
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -write -connection $POSTGIS_CONNECTION 
+
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -connection $POSTGIS_CONNECTION -deployproduction
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -connection $POSTGIS_CONNECTION -removebackup
+
