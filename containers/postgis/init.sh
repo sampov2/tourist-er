3442f79fe1ea5892d8be0c8b89aed5779f44eb58
Sampo Savolainen <sampo.savolainen@gmail.com>
2023-11-04 12:25:19 +0200

    wip: use imposm base mapping, refactor postgis container initialisation

--- containers/postgis/init.sh
+++ containers/postgis/init.sh
@@ -7,19 +7,19 @@ set -e
 echo 'localhost:5432:gis:docker:'$(cat /run/secrets/POSTGRES_PASS) > $HOME/.pgpass
 chmod 0600 $HOME/.pgpass
 
-wget -O /tmp/imposm.tar.gz https://github.com/omniscale/imposm3/releases/download/v$IMPOSM_VERSION/imposm-$IMPOSM_VERSION-linux-x86-64.tar.gz
+#wget -O /tmp/imposm.tar.gz https://github.com/omniscale/imposm3/releases/download/v$IMPOSM_VERSION/imposm-$IMPOSM_VERSION-linux-x86-64.tar.gz
 
 cd /tmp/
 tar xf imposm.tar.gz
 
 IMPOSM_DIR=/tmp/imposm-$IMPOSM_VERSION-linux-x86-64
-IMPOSM_MAPPING=$IMPOSM_DIR/mapping.json
+IMPOSM_MAPPING=/tmp/mapping.json
 
 POSTGIS_CONNECTION=postgis://docker:$(cat /run/secrets/POSTGRES_PASS)@localhost:5432/gis
 
-$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -read /tmp/input.pbf
-$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -write -connection $POSTGIS_CONNECTION 
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_MAPPING -read /tmp/input.pbf
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_MAPPING -write -connection $POSTGIS_CONNECTION 
 
-$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -connection $POSTGIS_CONNECTION -deployproduction
-$IMPOSM_DIR/imposm import -mapping $IMPOSM_DIR/mapping.json -connection $POSTGIS_CONNECTION -removebackup
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_MAPPING -connection $POSTGIS_CONNECTION -deployproduction
+$IMPOSM_DIR/imposm import -mapping $IMPOSM_MAPPING -connection $POSTGIS_CONNECTION -removebackup
 
